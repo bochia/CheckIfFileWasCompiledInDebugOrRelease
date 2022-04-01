252c49d1f031883eed79b955616587f3f64f91cf
ochiab <brandon.ochia@gmail.com>
2022-04-01 15:56:23 +0200

    first commit

---
+++ CheckIfFileWasCompiledInDebugOrRelease/AssemblyBuildModeChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfFileWasCompiledInDebugOrRelease
+{
+    class AssemblyBuildModeChecker
+    {
+
+        /// <summary>
+        /// Writes file build mode to console.
+        /// </summary>
+        /// <param name="file"></param>
+        public void CheckFileBuildMode(string file)
+        {
+            if (isAssemblyDebugBuild(file))
+            {
+                Console.WriteLine(String.Format("{0} seems to be a debug build", file));
+            }
+            else
+            {
+                Console.WriteLine(String.Format("{0} seems to be a release build", file));
+            }
+        }
+
+        private bool isAssemblyDebugBuild(string filename)
+        {
+            return isAssemblyDebugBuild(System.Reflection.Assembly.LoadFile(filename));
+        }
+
+        private bool isAssemblyDebugBuild(System.Reflection.Assembly assemb)
+        {
+            bool retVal = false;
+            foreach (object att in assemb.GetCustomAttributes(false))
+            {
+                if (att.GetType() == System.Type.GetType("System.Diagnostics.DebuggableAttribute"))
+                {
+                    retVal = ((System.Diagnostics.DebuggableAttribute)att).IsJITTrackingEnabled;
+                }
+            }
+            return retVal;
+        }
+    }
+}
---
+++ CheckIfFileWasCompiledInDebugOrRelease/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CheckIfFileWasCompiledInDebugOrRelease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                
+
+            var fileName = @"C:\Users\ochiab\Downloads\92769101_07Feb2022_KittingStation_StaticRunCode\BSC\Application\HMI\HMI.exe";
+
+            var fileBuildChecker = new AssemblyBuildModeChecker();
+
+            fileBuildChecker.CheckFileBuildMode(fileName);
+        }
+    }
+}
